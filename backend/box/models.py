978bab3b1af27cc1cf2c7a680525acfa40f8629a
maxim <urbaevmax@gmail.com>
2022-08-12 15:02:46 +0300

    test model and serializer

--- backend/box/models.py
+++ backend/box/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class PageModel(models.Model):
+    """ A test model for future REST API"""
+    image_url = models.CharField(max_length=200)
+    page_id = models.IntegerField()
+    video_duration = models.DateTimeField()
