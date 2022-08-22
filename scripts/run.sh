fc7f28a7bc60cf57a62f3b3199e87fe3987e6e1b
maxim <urbaevmax@gmail.com>
2022-08-22 17:17:39 +0300

    proxy image and bash scripts added

---
+++ scripts/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+whoami
+
+#python manage.py wait_for_db
+#python manage.py collectstatic --noinput
+python backend/manage.py makemigrations
+python backend/manage.py makemigrations box
+python backend/manage.py migrate
+python backend/manage.py migrate box
+
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module backend.wsgi
