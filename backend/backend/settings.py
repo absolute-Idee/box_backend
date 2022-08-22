773412ce2aa21d6994c027f453aae456383aa24d
maxim <urbaevmax@gmail.com>
2022-08-22 14:09:33 +0300

    variables hidden in ENV file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,9 +27,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None, 
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
