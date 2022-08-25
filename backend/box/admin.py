5b6d1944b500a78360992ec7f3a82bb5677abb97
maxim <urbaevmax@gmail.com>
2022-08-25 13:51:52 +0300

    models added into box.admin

--- backend/box/admin.py
+++ backend/box/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Trainer, Course, Training
 
 # Register your models here.
+admin.site.register(Trainer)
+admin.site.register(Course)
+admin.site.register(Training)
