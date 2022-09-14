36bb380cfb9a37cc03590b248912203694d48f0f
maxim <urbaevmax@gmail.com>
2022-09-14 18:53:33 +0300

    new tables added in admin panel

--- backend/box/admin.py
+++ backend/box/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Trainer, Course, Training, Messenger, TrainerMessenger
+from .models import Trainer, Course, Training, Messenger, TrainerMessenger, User, TrainingUser
 
 # Register your models here.
 admin.site.register(Trainer)
@@ -7,3 +7,5 @@ admin.site.register(Course)
 admin.site.register(Training)
 admin.site.register(Messenger)
 admin.site.register(TrainerMessenger)
+admin.site.register(User)
+admin.site.register(TrainingUser)
