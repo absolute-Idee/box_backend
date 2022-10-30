d566ee35a7a62073e304ad18f82cba411195cf38
maxim <urbaevmax@gmail.com>
2022-10-30 14:39:54 +0300

    new endpoints for id selection

--- backend/box/urls.py
+++ backend/box/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import GetCourseView, GetTrainerView
+from .views import GetCourseView, GetTrainerView, GetCourseByIdView, GetTrainerByIdView
 
 app_name = "box"
 
 urlpatterns = [
     path('GetCourse/', GetCourseView.as_view(), name='get_course'),
     path('GetTrainer/', GetTrainerView.as_view(), name='get_trainer'),
+    
+    path('GetCourse/<int:pk>', GetCourseByIdView.as_view(), name='get_course_by_id'),
+    path('GetTrainer/<int:pk>', GetTrainerByIdView.as_view(), name='get_trainer_by_id'),
 ]
--- backend/box/views.py
+++ backend/box/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import TrainerSerializer, CourseSerializer, UnfinishedCourseSerializer
 from .models import Course, Trainer
@@ -13,6 +14,7 @@ class GetCourseView(APIView):
 
     def get(self, request):
         a = FirebaseBackend.authenticate(self=self, request=request)
+        #a = True
         if a:
             course_true = Course.objects.filter(readiness=True)
             serializer_true = CourseSerializer(course_true, many=True)
@@ -25,14 +27,51 @@ class GetCourseView(APIView):
             return Response({'Error':'Wrong Auth'})
 
 
+class GetCourseByIdView(APIView):
+    """Course GET view for web version with id requirement"""
+
+    def get(self, request, pk):
+        a = FirebaseBackend.authenticate(self=self, request=request)
+        #a = True
+        if a:
+            try:
+                course = Course.objects.get(id=pk)
+            except:
+                return Response({'status': 'No course with given id'}, status=status.HTTP_404_NOT_FOUND)
+
+            serializer = CourseSerializer(course)
+            return Response(serializer.data)
+        else:
+            return Response({'Error':'Wrong Auth'})
+
+
 class GetTrainerView(APIView):
     """Trainer view"""
 
     def get(self, request):
         a = FirebaseBackend.authenticate(self=self, request=request)
+        #a = True
         if a:
             trainer = Trainer.objects.all()
             serializer = TrainerSerializer(trainer, many=True)
+            return Response(serializer.data)
+        else:
+            return Response({'Error':'Wrong Auth'})
+
+
+class GetTrainerByIdView(APIView):
+    """Course GET view for web version with id requirement"""
+
+    def get(self, request, pk):
+        a = FirebaseBackend.authenticate(self=self, request=request)
+        #a = True
+        if a:
+            try:
+                course = Trainer.objects.get(id=pk)
+            except:
+                return Response({'status': 'No course with given id'}, status=status.HTTP_404_NOT_FOUND)
+
+            serializer = TrainerSerializer(course)
             return Response(serializer.data)
         else:
             return Response({'Error':'Wrong Auth'})
